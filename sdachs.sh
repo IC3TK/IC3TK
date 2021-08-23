aac81ec5d74b2f2808a975c14c76d6666e27eed2
IC3TK <89388112+IC3TK@users.noreply.github.com>
2021-08-23 19:50:00 +0900

    Update sdachs.sh

--- sdachs.sh
+++ sdachs.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+# @(#) $Id$ GitHub workshop
 # only if geometry */255/63
 sudo fdisk -l /dev/sda | awk '/^\/dev\//{ printf "%s %d/%d/%d\n", $1, $2/(255*63), $2%(255*63)/63, $2%(255*63)%63+1 }'
